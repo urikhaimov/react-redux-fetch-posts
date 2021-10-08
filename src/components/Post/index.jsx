3cdb993af3c07ab33ef87c95c34edbb241c526ea
uri khaimov <urikhaimov@gmail.com>
2021-10-08 19:13:43 +0300

    infinitive scroll

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,19 +1,13 @@
 
+import './styles.css'
+
 const PostExcerpt = ({ post }) => {
     return (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <div>
-          <div/>
-          
-        </div>
-        <p className="post-content">{post.body.substring(0, 100)}</p>
-  
-      
-       
-      </article>
+        <article className="post-excerpt" key={post.id}>
+            <h3>{post.title}</h3>
+            <p className="post-content">{post.body.substring(0, 100)}</p>
+        </article>
     )
-  }
+}
 
-  export default PostExcerpt;
-  
+export default PostExcerpt;
--- src/components/PostsList/index.jsx
+++ src/components/PostsList/index.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectAllPosts, fetchPosts } from './../../features/posts/postsSlice';
 import PostExcerpt from './../Post'
-
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const PostsList = () => {
   const dispatch = useDispatch();
@@ -10,7 +10,15 @@ const PostsList = () => {
   console.log('posts', posts)
   const postStatus = useSelector(state => state.posts.status)
   const error = useSelector(state => state.posts.error)
-
+  const fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+     
+        posts =  posts.concat(Array.from({ length: 20 }))
+     
+    }, 1500);
+  };
   useEffect(() => {
     if (postStatus === 'idle') {
       dispatch(fetchPosts())
@@ -22,18 +30,40 @@ const PostsList = () => {
   if (postStatus === 'loading') {
     content = <div text="Loading..." />
   } else if (postStatus === 'succeeded') {
-    content = posts.map(post => (
-      <PostExcerpt key={post.id} post={post} />
-    ))
+    content =
+      <div
+        id="scrollableDiv"
+        style={{
+          height: '90vh',
+          overflow: 'auto',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+      
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={fetchMoreData}
+          style={{ display: 'flex', flexDirection: 'column' }} //To put endMessage and loader to the top.
+          inverse={true} //
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+         
+        >
+          {posts.map((post) => (
+            <PostExcerpt key={post.id} post={post} />
+          ))}
+        </InfiniteScroll>
+      </div>
+
   } else if (postStatus === 'failed') {
     content = <div>{error}</div>
   }
-
-  return (
-    <section className="posts-list">
-      <h2>Posts</h2>
-      {content}
-    </section>
+ 
+  return (<section className="posts-list">
+    <h2>Posts</h2>
+    {content}
+  </section>
   )
 }
 
